bc9fd9c3884c1a07f50f9e081b9fdd9657dbc72b
Jayshree Mhatre <71186667+jayshrimhatre7@users.noreply.github.com>
2022-07-07 21:26:05 +0530

    Add files via upload

---
+++ walmartdataset.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from matplotlib import pyplot as plt
+import seaborn as sns
+from sklearn import tree
+from sklearn import metrics
+from sklearn import preprocessing
+from sklearn.ensemble import RandomForestClassifier
+import json
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn import metrics
+from sklearn.linear_model import LinearRegression
+
+train=pd.read_csv('C:/Users/hp/Desktop/datasets/walmart_train.csv')
+test=pd.read_csv('C:/Users/hp/Desktop/datasets/walmart_test.csv')
+
+train.isnull().sum()
+
+X = train[['Dept','IsHoliday']].loc[ (train['Store']==1) ]
+Y = train[['Weekly_Sales']].loc[ (train['Store']==1) ]
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y)
+
+le=LabelEncoder()
+le.fit(Y_train)
+
+LinReg = LinearRegression()
+LinReg.fit(X_train, Y_train)
+
+train = LinReg.fit(X_train,Y_train)
+Y_pred=LinReg.predict(X_test)
+print(LinReg.score(X_test, Y_test))
+
+print(f'Intercept = {LinReg.intercept_} \nCoefficient = {len(LinReg.coef_)}')
+
+print(f'Mean Squared Error = {np.mean((LinReg.predict(X_test)-Y_test)**2)}' + f'\nVariance Score = {LinReg.score(X_test,Y_test)}')
+
+print(f'Mean Squared Error = {np.sqrt(metrics.mean_squared_error(LinReg.predict(X_test),Y_test))}')
